2f27a7f84fb76727836b1834f29bce4f468d8a5a
the-real-wizhack <54763483+the-real-wizhack@users.noreply.github.com>
2021-02-22 10:19:04 +0100

    Update freebox.py

--- freebox.py
+++ freebox.py
@@ -5,10 +5,10 @@ from urllib.request import urlopen,Request
 from socket import timeout
 
 class FbxCnx:
-    def __init__(self,host="mafreebox.freebox.fr"):
+    def __init__(self,apiv,host="mafreebox.freebox.fr"):
         self.host=host
-        url_get = requests.get(self.host+"/api_version")
-        apiv = url_get.json()["api_version"][0:1]
+        apiv_get = requests.get(self.host+"/api_version")
+        apiv = apiv_get.json()["api_version"][0:1]
 
     def register(self,appid,appname,version,devname):
         data={'app_id': appid,'app_name': appname,'app_version':version,'device_name': devname}
